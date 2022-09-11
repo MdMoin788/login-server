69f3c59e36b8929ff1569dd9dd7b177d159261a3
MdMoin788 <developermdmoin@gmail.com>
2022-09-11 08:34:40 +0530

    login

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+app.use(express.json());
+app.use(cors())
+require("dotenv").config()
+//
+const connect = require("./config/db")
+
+
+const {register,login} = require("./controllers/userController");
+
+app.post("/register",register)
+app.post("/login",login)
+
+app.listen( process.env.PORT||5000, async()=>{
+    try {
+        await connect();
+        console.log('Server Connected Success')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
